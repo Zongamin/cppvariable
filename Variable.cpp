aa1b4a932cbf5e3fdf395744f28bb01c99c90c16
Zongamin <dokbasti77@gmail.com>
2024-02-04 18:11:17 +0100

    Gesundheit hinzugefügt

---
+++ Variable.cpp
@@ -0,0 +1,17 @@
+// String 
+// int
+// float
+// bool
+
+#include <string.h>
+#include <iostream>
+
+int Gesundheit = 100;
+
+int main()
+{
+    
+    std::cout << "Gesundheit : " << + Gesundheit;
+    return 0;
+    
+}
