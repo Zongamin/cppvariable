6e6ba4b97a34182f4b152cc04e32f27642d58a83
Zongamin <dokbasti77@gmail.com>
2024-02-04 18:34:11 +0100

    Ges - 10

--- Variable.cpp
+++ Variable.cpp
@@ -8,10 +8,25 @@
 
 int Gesundheit = 100;
 
+int output ()
+{
+    std::cout << "Gesundheit : " << + Gesundheit << "\n";
+    return 0;
+}
+int gesMinus ()
+{
+    Gesundheit --;
+    return 0;
+}
+
 int main()
 {
+    for (int i = 0; i < 11; i++)
+    {
+    output(); 
+    gesMinus();
+    }
     
-    std::cout << "Gesundheit : " << + Gesundheit;
     return 0;
-    
+
 }
